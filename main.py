7b566c605474e3af67745bf0d9a5b45de420b327
malcano <steveleemh@naver.com>
2023-01-18 11:44:08 +0900

    add some comments for main

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def load_module(module_number):
     mod = importlib.import_module("sol"+str(module_number))
     return mod
 
+# 모듈을 동적으로 호출
+# solution number만 변경하면 main을 실행할 수 있음
+
 #Do not Edit this code!
 if __name__ == '__main__':
 
--- sol1018.py
+++ sol1018.py
@@ -3,9 +3,10 @@ import sys
 
 def solution():
     N, M = map(int, input().split())
+    board = []
+    for _ in range(M):
+        board.append(input())
 
-
-
-    chessboard = [list(map(str, sys.stdin.readline().split())) for _ in range(N)]
-    print([_ for _ in chessboard])
+    #chessboard = [list(map(str, sys.stdin.readline().split())) for _ in range(N)]
+    print([_ for _ in board])
     # for _ in chessboard:
