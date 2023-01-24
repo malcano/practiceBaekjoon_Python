e958c5498a4d33224e6182495cc312dbddcd7876
malcano <steveleemh@naver.com>
2023-01-24 18:23:37 +0900

    solve 10814

--- sol10814.py
+++ sol10814.py
@@ -1,1 +1,15 @@
-# sol
+# sol 10814
+import sys
+def solution():
+    input = sys.stdin.readline
+    N = int(input())
+    name_list = []
+
+    for _ in range(N):
+        age, name = map(str, input().split())
+        name_list.append([int(age), name])
+
+    name_list = list(sorted(name_list, key=lambda x : x[0]))
+
+    for _ in name_list:
+        print(f"{_[0]} {_[1]}")
